2eb989f803e7d747b885a713da927d1cc01ecfdf
Pierre-Michel Brown <pierremichelbrown@gmail.com>
2023-02-28 16:31:29 -0500

    Test waiting for async data

---
+++ pages/test.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const data = ref('')
+
+await new Promise<void>(resolve => {
+  setTimeout(() => {
+    data.value = 'Some data'
+    resolve()
+  }, 1000)
+})
+</script>
+
+<template>
+  <div v-if="data">
+    {{ data }}
+  </div>
+</template>
